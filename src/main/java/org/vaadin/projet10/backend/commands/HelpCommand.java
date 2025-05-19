fbe29c8e0d2db9c6bd886a922ffeda7bd5287658
Elodie TRAN <elodie.tran0904@gmail.com>
2025-05-19 15:21:31 +1100

    refactor: rename 'mastodonte' command to 'search' with alias

--- src/main/java/org/vaadin/projet10/backend/commands/HelpCommand.java
+++ src/main/java/org/vaadin/projet10/backend/commands/HelpCommand.java
@@ -4,12 +4,12 @@ public class HelpCommand implements Command {
     @Override
     public String execute(String[] args) {
         return String.format("""
-            Help Command :
-            - mastodonte <hashtag> : récupère le plus populaire.
-                args :  -v        : affiche les dates des posts.
-                        -d <int>  : récupère les post dans les <int> jours.
-            - clear : Permet d'effacer le terminal.
-            - mshow <hashtag> <numéro de post>  : Permet d'afficher le post en entier.
-        """);
+        Help Command :
+        - search <hashtag> : récupère le plus populaire. (alias : mastodonte)
+            args :  -v        : affiche les dates des posts.
+                    -d <int>  : récupère les post dans les <int> jours.
+        - clear : Permet d'effacer le terminal.
+        - mshow <hashtag> <numéro de post>  : Permet d'afficher le post en entier.
+    """);
     }
 }
